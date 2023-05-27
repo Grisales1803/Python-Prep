297ee38481507024ec773fb9eab3f94b49e61e35
Grisales1803 <grisales1803@gmail.com>
2023-05-27 12:14:43 -0500

    Cambio1

--- M03_variablesydatos/Prep_Course_Homework_03.py
+++ M03_variablesydatos/Prep_Course_Homework_03.py
@@ -7,14 +7,15 @@
 
 # In[7]:
 
-
-
+x = 7
+print(x)
 
 # 2) Imprimir el tipo de dato de la constante 8.5
 
 # In[3]:
 
 
+print(type(8.5))
 
 
 
@@ -23,29 +24,28 @@
 # In[8]:
 
 
-
+print(type(x))
 
 
 # 4) Crear una variable que contenga tu nombre
 
 # In[2]:
 
-
+nombre = "Juan"
 
 
 # 5) Crear una variable que contenga un número complejo
 
 # In[3]:
 
-
-
+complejo = 5+6j
 
 
 # 6) Mostrar el tipo de dato de la variable crada en el punto 5
 
 # In[4]:
 
-
+print(type(complejo))
 
 
 
@@ -61,14 +61,16 @@ pi = 3.1416
 
 # In[3]:
 
+valorA = "True"
+valorB = True
 
 
-
-
-# 9) Imprimir el tipo de dato correspondientes a las variables creadas en el punto 9
+# 9) Imprimir el tipo de dato correspondientes a las variables creadas en el punto 8
 
 # In[5]:
 
+print(type(valorA))
+print(type(valorB))
 
 
 
@@ -77,7 +79,7 @@ pi = 3.1416
 
 # In[1]:
 
-
+suma = 8 + 8.5
 
 
 
@@ -85,23 +87,24 @@ pi = 3.1416
 
 # In[2]:
 
+i = 6+6j
+j= 7+4j
 
-
+sumaComplex = i+j
 
 
 # 12) Realizar una operación de suma de un número real y otro complejo
 
 # In[4]:
 
-
-
+SumaRealyComplex = 5 + 5j
 
 
 # 13) Realizar una operación de multiplicación
 
 # In[5]:
 
-
+print(5*5)
 
 
 
@@ -109,22 +112,23 @@ pi = 3.1416
 
 # In[6]:
 
-
+print(2**3)
 
 
 # 15) Obtener el cociente de la división de 27 entre 4 en una variable y luego mostrarla
 
 # In[8]:
 
-
-
+Cociente = 27/4
+print(Cociente)
 
 
 # 16) De la división anterior solamente mostrar la parte entera
 
 # In[9]:
 
-
+Entero = 27//4
+print(Entero)
 
 
 
@@ -132,15 +136,15 @@ pi = 3.1416
 
 # In[1]:
 
-
-
+Residuo = 27%4
+print(Residuo)
 
 
 # 18) Utilizando como operandos el número 4 y los resultados obtenidos en los puntos 16 y 17. Obtener 27 como resultado
 
 # In[2]:
 
-
+print(Entero*4+Residuo)
 
 
 
@@ -148,7 +152,7 @@ pi = 3.1416
 
 # In[3]:
 
-
+print("Hola" + " Mundo")
 
 
 
@@ -157,14 +161,14 @@ pi = 3.1416
 # In[4]:
 
 
-
+print(bool("2" == 2))
 
 
 # 21) Utilizar las funciones de cambio de tipo de dato, para que la validación del punto 20 resulte verdadera
 
 # In[11]:
 
-
+print(bool(int("2") == 2))
 
 
 
@@ -173,22 +177,23 @@ pi = 3.1416
 # In[12]:
 
 
-
-
+a = float('3.8')
+print(a)
 
 # 23) Crear una variable con el valor 3, y utilizar el operador '-=' para modificar su contenido
 
 # In[15]:
 
-
-
+variable = 3
+variable -= 3
+print(variable)
 
 
 # 24) Realizar la operacion 1 << 2 ¿Por qué da ese resultado? ¿Qué es el sistema de numeración binario?
 
 # In[29]:
 
-
+print(1 << 2)
 
 
 
